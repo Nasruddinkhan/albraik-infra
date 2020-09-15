7e232a7efd3ce9e325eef550586bd6aaaea99ccb
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-09-15 21:25:02 +0530

    remove imports

--- src/main/java/com/albraik/infra/feign/service/MailSenderFeignService.java
+++ src/main/java/com/albraik/infra/feign/service/MailSenderFeignService.java
@@ -1,7 +1,6 @@
 package com.albraik.infra.feign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
