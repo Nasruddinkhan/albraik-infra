ee549eb4d2f6b2392e860faa04775a86c12ff9cc
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-10-03 21:54:25 +0530

    change project type controller

--- src/main/java/com/albraik/infra/project/controller/ProjectTypeController.java
+++ src/main/java/com/albraik/infra/project/controller/ProjectTypeController.java
@@ -3,7 +3,6 @@ package com.albraik.infra.project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +12,18 @@ import com.albraik.infra.project.model.ProjectTypeEntity;
 import com.albraik.infra.project.service.ProjectTypeService;
 
 @RestController
-@RequestMapping("/api/project/type")
+@RequestMapping("/api")
 public class ProjectTypeController {
 
 	@Autowired
 	private ProjectTypeService projectTypeService;
 
 
-	@GetMapping
+	@GetMapping("/project/type")
 	public ResponseEntity<List<ProjectTypeEntity>> getAllProjectTypes() {
 		List<ProjectTypeEntity> projectTypes = projectTypeService.getAllProjectType();
 		if (projectTypes.isEmpty())
-			return new ResponseEntity<>(projectTypes, HttpStatus.NO_CONTENT);
+			throw new RuntimeException("No content found");
 		return ResponseEntity.ok(projectTypes);
 	}
 
